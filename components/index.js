641f0638db25f3d2ea82b42ff842c76d8a79ea2c
harikrishnan <hari@calibr.ai>
2024-03-28 15:54:59 +0530

    discover page added.

--- components/index.js
+++ components/index.js
@@ -3,5 +3,13 @@ import AuthHeader from "./AuthHeader";
 import Loader from "./Loader";
 import FormInput from "./FormInput";
 import CustomButton from "./CustomButton";
+import SideBar from "./SideBar";
 
-export { RootLayoutWrapper, AuthHeader, Loader, FormInput, CustomButton };
+export {
+  RootLayoutWrapper,
+  AuthHeader,
+  Loader,
+  FormInput,
+  CustomButton,
+  SideBar,
+};
